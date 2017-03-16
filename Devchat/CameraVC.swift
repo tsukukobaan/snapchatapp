05b05a1696dcd0959e30edcc12b083743f33f9c1
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-03-16 11:27:47 +0900

    migration with sample codes

---
+++ Devchat/CameraVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Devchat
+//
+//  Created by 小林 泰 on 2017/03/16.
+//  Copyright © 2017年 TokyoIceHockeyChannel. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: AVCamCameraViewController {
+
+    @IBOutlet weak var previewView: AVCamPreviewView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
